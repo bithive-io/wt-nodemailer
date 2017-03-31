be062b3618b2518243837b19195a6e28d784ea94
Diego Rojas <diegoluis.rojas@gmail.com>
2017-03-31 22:13:41 +0800

    final fix

--- express.js
+++ express.js
@@ -18,21 +18,22 @@ app.post('/mailer/echo', function (req, res) {
 
 app.post('/mailer/hire', function (req, res) {
 
-    var content = "- ORGANIZATION:" + req.body.org + "\n" + "- MESSAGE: \n" + req.body.message;
+  var subject = 'New HIRE Message from ' + req.body.name + ' <' + req.body.email +'>';
+  var content = "- ORGANIZATION:" + req.body.org + "\n" + "- MESSAGE: \n" + req.body.message;
 
-    var Sendgrid = require('sendgrid')(req.webtaskContext.secrets.SG_KEY);
+  var Sendgrid = require('sendgrid')(req.webtaskContext.secrets.SG_KEY);
 
-    Sendgrid.send({
-        to: req.webtaskContext.secrets.SG_TO,
-        from: req.webtaskContext.secrets.SG_FROM,
-        subject: 'New HIRE Message from ' + req.body.name + ' <' + req.body.email +'>',
-        text: content
-    }, function(err, json) {
-        if(err)
-          res.status(500).send(err);
-        else
-          res.status(200).send("ok");
-    });
+  Sendgrid.send({
+    to: req.webtaskContext.secrets.SG_TO,
+    from: req.webtaskContext.secrets.SG_FROM,
+    subject: subject,
+    text: content
+  }, function(err, json) {
+    if(err)
+    res.status(500).send(err);
+    else
+    res.status(200).send("ok");
+  });
 });
 
 
@@ -40,29 +41,30 @@ app.post('/mailer/hire', function (req, res) {
 
 app.post('/mailer/join', function (req, res) {
 
-    var content = "- NAME:" + req.params.name + "\n" +
-                  "- EMAIL: " + req.params.email + "\n" +
-                  "- AVAILABILITY (hs): " + req.params.availability + "\n" +
-                  "- LOCATION: " + req.params.location + "\n" +
-                  "- SKILL: " + req.params.skill + "\n" +
-                  "- WEBSITE: " + req.params.website + "\n" +
-                  "- LINKEDIN: " + req.params.linkedin + "\n" +
-                  "- GITHUB: " + req.params.github + "\n" +
-                  "- MESSAGE: \n" + req.params.message;
+  var subject = 'New JOIN Message from ' + req.params.name + ' <' + req.params.email +'>';
+  var content = "- NAME:" + req.params.name + "\n" +
+  "- EMAIL: " + req.params.email + "\n" +
+  "- AVAILABILITY (hs): " + req.params.availability + "\n" +
+  "- LOCATION: " + req.params.location + "\n" +
+  "- SKILL: " + req.params.skill + "\n" +
+  "- WEBSITE: " + req.params.website + "\n" +
+  "- LINKEDIN: " + req.params.linkedin + "\n" +
+  "- GITHUB: " + req.params.github + "\n" +
+  "- MESSAGE: \n" + req.params.message;
 
-    var Sendgrid = require('sendgrid')(req.webtaskContext.secrets.SG_KEY);
+  var Sendgrid = require('sendgrid')(req.webtaskContext.secrets.SG_KEY);
 
-    Sendgrid.send({
-        to: req.webtaskContext.secrets.SG_TO,
-        from: req.webtaskContext.secrets.SG_FROM
-        subject: 'New JOIN Message from ' + req.params.name + ' <' + req.params.email +'>',
-        text: content
-    }, function(err, json) {
-        if(err)
-          res.status(500).send(err);
-        else
-          res.status(200).send("ok");;
-    });
+  Sendgrid.send({
+    to: req.webtaskContext.secrets.SG_TO,
+    from: req.webtaskContext.secrets.SG_FROM,
+    subject: subject,
+    text: content
+  }, function(err, json) {
+    if(err)
+    res.status(500).send(err);
+    else
+    res.status(200).send("ok");;
+  });
 })
 
 
