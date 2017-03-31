072ad7f4f83f26469586060caadee4ac612290cf
Diego Rojas <diegoluis.rojas@gmail.com>
2017-03-31 21:51:04 +0800

    minor

--- express.js
+++ express.js
@@ -1,6 +1,5 @@
-var Express = require('express');
+var app = new(require('express'));
 var Webtask = require('webtask-tools');
-var app = Express();
 
 //app.use(require('body-parser').json());
 var bodyParser = require('body-parser')
@@ -9,12 +8,8 @@ app.use(express.bodyParser());
 // [POST] hire
 
 app.post('/mailer/hire', function (req, res) {
-    // if((!req.webtaskContext.data.SG_KEY) ||
-    //    (!req.webtaskContext.data.SG_TO)  ||
-    //    (!req.webtaskContext.data.SG_FROM))
-    //      res.status(500).send("error");
 
-    var content = "- ORGANIZATION:" + req.body.org + "\n" + "- MESSAGE: " + "\n" + req.body.message;
+    var content = "- ORGANIZATION:" + req.body.org + "\n" + "- MESSAGE: \n" + req.body.message;
 
     var Sendgrid = require('sendgrid')(req.webtaskContext.data.SG_KEY);
 
@@ -35,10 +30,6 @@ app.post('/mailer/hire', function (req, res) {
 // [POST] join
 
 app.post('/mailer/join', function (req, res) {
-    if((!req.webtaskContext.data.SG_KEY) ||
-       (!req.webtaskContext.data.SG_TO)  ||
-       (!req.webtaskContext.data.SG_FROM))
-         res.status(500).send("error");
 
     var content = "- NAME:" + req.params.name + "\n" +
                   "- EMAIL: " + req.params.email + "\n" +
