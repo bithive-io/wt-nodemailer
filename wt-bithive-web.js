b135af888f9c8eab43f1855b571b09298d7c30da
Diego Rojas <diegoluis.rojas@gmail.com>
2017-03-31 22:25:34 +0800

    minor

--- wt-bithive-web.js
+++ wt-bithive-web.js
@@ -2,7 +2,7 @@ var app = new(require('express'));
 var Webtask = require('webtask-tools');
 
 app.post('/mailer/echo', function (req, res) {
-  res.status(200).send(req.webtaskContext.body);
+  res.status(200).send(req.webtaskContext.data.name);
 });
 
 // [POST] hire
