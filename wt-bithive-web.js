3d26a7d65ab6dc8bf9ce49b1331f6f8689371349
Diego Rojas <diegoluis.rojas@gmail.com>
2017-04-01 11:47:06 +0800

    fixing format

--- wt-bithive-web.js
+++ wt-bithive-web.js
@@ -1,17 +1,16 @@
 var app = new(require('express'));
 var Webtask = require('webtask-tools');
 
-const SG_BR = '=0D';
-
 app.use(require('body-parser').json());
 
-
 // [POST] hire
-
 app.post('/mailer/hire', function (req, res) {
 
-  var subject = 'New HIRE Message from ' + req.body.name + ' <' + req.body.email +'>';
-  var content = 'ORGANIZATION: ' + req.body.org + SG_BR + 'MESSAGE: ' + SG_BR + req.body.message;
+  var subject = 'New HIRE Message for Bithive';
+  var content = '- NAME: ' + req.body.name +
+                '- EMAIL: ' + req.body.email +
+                '- ORGANIZATION: ' + req.body.org  +
+                '- MESSAGE: ' + req.body.message;
 
   var Sendgrid = require('sendgrid')(req.webtaskContext.secrets.SG_KEY);
 
@@ -30,19 +29,18 @@ app.post('/mailer/hire', function (req, res) {
 
 
 // [POST] join
-
 app.post('/mailer/join', function (req, res) {
 
-  var subject = 'New JOIN Message from ' + req.body.name + ' <' + req.body.email +'>';
-  var content = "NAME:" + req.body.name + SG_BR +
-  "EMAIL: " + req.body.email + SG_BR +
-  "AVAILABILITY (hs): " + req.body.availability + SG_BR +
-  "LOCATION: " + req.body.location + SG_BR +
-  "SKILL: " + req.body.skill + SG_BR +
-  "WEBSITE: " + req.body.website + SG_BR +
-  "LINKEDIN: " + req.body.linkedin + SG_BR +
-  "GITHUB: " + req.body.github + SG_BR +
-  "MESSAGE: " + SG_BR + req.body.message;
+  var subject = 'New JOIN Message for Bithive';
+  var content = "- NAME:" + req.body.name +
+                "- EMAIL: " + req.body.email  +
+                "- AVAILABILITY (hs): " + req.body.availability +
+                "- LOCATION: " + req.body.location +
+                "- SKILL: " + req.body.skill  +
+                "- WEBSITE: " + req.body.website +
+                "- LINKEDIN: " + req.body.linkedin +
+                "- GITHUB: " + req.body.github +
+                "- MESSAGE: " + req.body.message;
 
   var Sendgrid = require('sendgrid')(req.webtaskContext.secrets.SG_KEY);
 
@@ -58,6 +56,5 @@ app.post('/mailer/join', function (req, res) {
       res.status(200).json({ success : true });
   });
 })
-
 
 module.exports = Webtask.fromExpress(app);
